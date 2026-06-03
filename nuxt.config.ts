f87960efb69d680b8e401fe0eef54c814d8465a0
Mateus <mateus.castrow@hotmail.com>
2026-06-03 20:10:11 -0300

    ADD FAVICON

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      title: 'Guimarães Advocacia',
+      link: [
+        { rel: 'icon', type: 'image/webp', href: '/favicon.webp' }
+      ]
+    }
+  }
 })
